bd142c86689c572917b2497a8a6e3b82ddac7278
gera13sa <93113717+gera13sa@users.noreply.github.com>
2023-09-11 21:19:21 +0300

    modified:   src/app/second-component/second-component.component.ts
    modified:   src/app/app.component.html
    modified:   src/app/app.component.spec.ts
    modified:   src/app/app.module.ts
    deleted:    src/app/second-component/second-component.component.css
    deleted:    src/app/second-component/second-component.component.html
    deleted:    src/app/second-component/second-component.component.spec.ts
    deleted:    src/app/second-component/second-component.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SecondComponentComponent } from './second-component/second-component.component';
 
 @NgModule({
   declarations: [	
-    AppComponent,
-      SecondComponentComponent
+    AppComponent
    ],
   imports: [
     BrowserModule
