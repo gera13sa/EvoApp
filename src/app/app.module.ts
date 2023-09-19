dbacac51c7beba601125efd0a7f6ed94e1d42534
gera13sa <93113717+gera13sa@users.noreply.github.com>
2023-09-20 00:19:46 +0300

    modified:   src/app/app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MainComponent } from './main/main.component';
 import { PageModule } from './page/page.module';
 import { MatTabsModule } from '@angular/material/tabs';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PageRoutingModule } from './page/page-routing.module';
+
 
 @NgModule({
   declarations: [		
