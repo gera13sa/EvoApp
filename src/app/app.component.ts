d3854ebbaca88bc062eeb09bdd2b36e08b4247e3
gera13sa <93113717+gera13sa@users.noreply.github.com>
2023-09-24 00:50:27 +0300

    modified:   angular.json modified:   package-lock.json modified:   package.json modified:   src/app/app.component.css modified:   src/app/app.component.html deleted:    src/app/app.component.spec.ts modified:   src/app/app.component.ts modified:   src/app/app.module.ts modified:   src/index.html modified:   src/styles.css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Notify } from 'notiflix';
+import * as dayjs from 'dayjs';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +9,72 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  textToDisplay = 'hello';
+
+  alertsArray = [
+    {
+      type: "success", 
+      text: 'Успех',
+      tooltip: 'Вывести сообщение об успешной операции'
+    }, 
+    {
+      type: "failure", 
+      text: 'Неудача',
+      tooltip: 'Вывести сообщение о неудачной операции'
+    },
+    {
+      type: "info", 
+      text: 'Информация',
+      tooltip: 'Вывести информационное сообщение'
+    }
+  ];
+
+  bsRangeValue: Date[];
+  currentDate = new Date();
+
+  buttonDisabled = false;
+
+  bootstrapAlerts: {type: string, text: String, timeout: number}[] = [];
+
+  notiflix_alert(alertType: String): void{
+    switch(alertType){
+      case "success":
+        Notify.success('Operation succeeded!');
+        break;
+      case "failure":
+        Notify.failure("Operation failed!");
+        break;
+     case "info":
+        Notify.info("Important info!");
+        break;
+    };
+  }
+
+  check_date(): void{
+    const startDate = dayjs(this.bsRangeValue[0]);
+    const endDate = dayjs(this.bsRangeValue[1]);
+    const daysDifference = endDate.diff(startDate, 'days');
+
+    if(daysDifference > 7) {
+      this.bootstrapAlerts.push({
+        type: 'danger',
+        text: `Размечтались, нельзя брать отпуск более 7 дней!`,
+        timeout: 6000
+      });
+      return;
+    }
+
+    this.bootstrapAlerts.push({
+      type: 'success',
+      text: `Вы успешно взяли отпуск на ${daysDifference} дней!`,
+      timeout: 0
+    });
+    this.buttonDisabled = true;
+  }
+
+  constructor() {
+    var tempDate = new Date(this.currentDate);
+    tempDate.setDate(tempDate.getDate() + 7);
+    this.bsRangeValue = [new Date(), tempDate];
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MatButtonModule} from '@angular/material/button';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatIconModule} from '@angular/material/icon'
+
+import { AlertModule } from 'ngx-bootstrap/alert';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [	
     AppComponent
    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule,
+    MatExpansionModule, 
+    MatTooltipModule,
+    MatIconModule,
+    BsDatepickerModule,
+    ButtonsModule,
+    AlertModule
   ],
   providers: [],
   bootstrap: [AppComponent]
