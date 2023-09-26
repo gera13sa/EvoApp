7baf95b4c94fcc5a3551e71be370fc31e0259fbf
gera13sa <93113717+gera13sa@users.noreply.github.com>
2023-09-26 23:30:38 +0300

    modified:   src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ResponseService } from './response.service';
 import { IResponse } from './response-interface';
 
